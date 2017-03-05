427a1bd72a674aa66f64ddc8c4bd79e93f1c877d
Akiicat <aaaa1379@gmail.com>
2017-03-06 07:11:45 +0800

    Submit message and fix top navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "chatrooms#index"
   resources :chatrooms do
     resource :chatroom_users
+    resources :messages
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
