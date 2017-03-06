ae0fe12cb44782d0e61ea028df0a9abff5f97582
Akiicat <aaaa1379@gmail.com>
2017-03-07 05:15:44 +0800

    broadcast_to method

--- app/channels/chatrooms_channel.rb
+++ app/channels/chatrooms_channel.rb
@@ -1,6 +1,7 @@
 class ChatroomsChannel < ApplicationCable::Channel
   def subscribed
     current_user.chatrooms.each do |chatroom|
+      # stream_for chatroom
       stream_from "chatrooms:#{chatroom.id}"
     end
     # stream_from "some_channel"
--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -2,6 +2,9 @@ class MessageRelayJob < ApplicationJob
   queue_as :default
 
   def perform(message)
+    # ChatroomsChannel.broadcast_to message.chatroom,
+    #   message: MessagesController.render(message),
+    #   chatroom_id: message.chatroom.id
     ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}",
       message: MessagesController.render(message),
       chatroom_id: message.chatroom.id
